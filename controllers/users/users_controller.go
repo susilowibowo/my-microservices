7012110f41250862bd6753934712c7a0259edae9
susilo wibowo <susilohdwibowo@gmail.com>
2020-05-28 09:05:22 +0700

    update 1

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,30 +4,27 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/susilowibowo/my-microservices/domain/users"
-	"github.com/susilowibowo/my-microservices/services"
-	"github.com/susilowibowo/my-microservices/utils/errors"
 )
 
-func CreateUser(c *gin.Context) {
-	var user users.User
+// func CreateUser(c *gin.Context) {
+// 	var user users.User
 
-	if err := c.ShouldBindJSON(&user); err != nil {
-		//TODO: return bad request to the caller.
-		restErr := errors.NewBadRequestError("invalid JSON body")
-		c.JSON(restErr.Status, restErr)
-		return
-	}
-	result, saveErr := services.CreateUser(user)
+// 	if err := c.ShouldBindJSON(&user); err != nil {
+// 		//TODO: return bad request to the caller.
+// 		restErr := errors.NewBadRequestError("invalid JSON body")
+// 		c.JSON(restErr.Status, restErr)
+// 		return
+// 	}
+// 	result, saveErr := services.CreateUser(user)
 
-	if saveErr != nil {
-		//TODO: handle user creation error
-		c.JSON(saveErr.Status, saveErr)
-		return
-	}
+// 	if saveErr != nil {
+// 		//TODO: handle user creation error
+// 		c.JSON(saveErr.Status, saveErr)
+// 		return
+// 	}
 
-	c.JSON(http.StatusCreated, result)
-}
+// 	c.JSON(http.StatusCreated, result)
+// }
 
 func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "not implemented")
